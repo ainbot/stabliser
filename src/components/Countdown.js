110f379a592fdd154b565508df37e165764222a8
Justino José <sdankhey848@gmail.com>
2024-06-01 16:48:06 +0200

    Descrição das alterações feitas

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -17,7 +17,9 @@ const Countdown = () => {
             messagingSenderId: "1002481110551",
             appId: "1:1002481110551:web:e7a439f4b87aeaff2b1652"
         };
-        firebase.initializeApp(firebaseConfig);
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
 
         // Referência para o nó do banco de dados
         const dbRef = firebase.database().ref('/id-log');
@@ -28,22 +30,31 @@ const Countdown = () => {
                 // Obter os dados atuais do Firebase
                 dbRef.child(uniqueId).once('value', snapshot => {
                     const time = snapshot.child('time').val();
-                    const [hours, minutes] = time.split(':');
-                    let totalMinutes = parseInt(hours, 10) * 60 + parseInt(minutes, 10);
+                    if (time) { // Verificação adicionada
+                        const [hours, minutes] = time.split(':').map(Number);
+                        if (isNaN(hours) || isNaN(minutes)) {
+                            clearInterval(interval);
+                            return;
+                        }
 
-                    // Diminuir um minuto
-                    totalMinutes--;
+                        let totalMinutes = hours * 60 + minutes;
 
-                    // Convertendo de volta para o formato de hora (HH:mm)
-                    const newHours = Math.floor(totalMinutes / 60);
-                    const newMinutes = totalMinutes % 60;
-                    const newTime = `${newHours < 10 ? '0' + newHours : newHours}:${newMinutes < 10 ? '0' + newMinutes : newMinutes}`;
+                        // Diminuir um minuto
+                        totalMinutes--;
 
-                    // Atualizando o nó com o novo valor de tempo
-                    dbRef.child(`${uniqueId}/time`).set(newTime);
+                        // Convertendo de volta para o formato de hora (HH:mm)
+                        const newHours = Math.floor(totalMinutes / 60);
+                        const newMinutes = totalMinutes % 60;
+                        const newTime = `${newHours < 10 ? '0' + newHours : newHours}:${newMinutes < 10 ? '0' + newMinutes : newMinutes}`;
 
-                    // Se o tempo chegar a 00:00, pare o intervalo
-                    if (totalMinutes === 0) {
+                        // Atualizando o nó com o novo valor de tempo
+                        dbRef.child(`${uniqueId}/time`).set(newTime);
+
+                        // Se o tempo chegar a 00:00, pare o intervalo
+                        if (totalMinutes <= 0) {
+                            clearInterval(interval);
+                        }
+                    } else {
                         clearInterval(interval);
                     }
                 });
@@ -56,10 +67,12 @@ const Countdown = () => {
             snapshot.forEach(childSnapshot => {
                 const uniqueId = childSnapshot.key;
                 const time = childSnapshot.child('time').val();
-                countdownsData.push({ uniqueId, time });
+                if (time) { // Verificação adicionada
+                    countdownsData.push({ uniqueId, time });
 
-                // Iniciar contagem regressiva para cada hora
-                updateCountdown(uniqueId, time);
+                    // Iniciar contagem regressiva para cada hora
+                    updateCountdown(uniqueId, time);
+                }
             });
             setCountdowns(countdownsData);
         });
@@ -70,20 +83,48 @@ const Countdown = () => {
         };
     }, []);
 
-    const handleRefreshClick = () => {
-        // Atualiza a página quando o botão de atualização é clicado
-        window.location.reload();
+    const handleWhatsappClick = () => {
+        // Abre o link do WhatsApp ao clicar no botão
+        window.open('https://wa.link/199y22', '_blank');
+    };
+
+    const handleDownloadClick = () => {
+        const dbRef = firebase.database().ref('/linkdown');
+        dbRef.once('value', snapshot => {
+            const downloadLink = snapshot.val();
+            if (downloadLink) {
+                window.open(downloadLink, '_blank');
+            } else {
+                alert('Link de download não disponível.');
+            }
+        });
     };
 
     return (
-        <div className="container"> {/* Adicionando a classe container */}
-            <h1>Contagem Regressiva</h1>
-            <div>
-                {countdowns.map(({ uniqueId, time }) => (
-                    <div key={uniqueId}>{uniqueId}: {time}</div>
-                ))}
+        <div className="container">
+            <h1 className="title">Web in Build</h1>
+            <div className="new-elements-container">
+                <div className="image-container">
+                    <img src="/images/bg.png" alt="Imagem" className="main-image" />
+                </div>
+                <div className="text-container">
+                    <p style={{ color: 'white', textAlign: 'justify', paddingLeft: '20px', paddingRight: '20px', marginBottom: '20px' }}>
+                        Ao iniciar o aplicativo, você será solicitado a conceder as permissões necessárias, como acesso à internet e serviços de localização, garantindo que o NetStabiliser funcione corretamente.
+                        Com um simples toque no interruptor, você pode iniciar o processo de teste de rede. O aplicativo executa testes de ping para múltiplos destinos e registra os resultados em tempo real.
+                        Todos os resultados dos testes são exibidos em um log detalhado, acessível dentro do aplicativo. Você pode percorrer o log para ver o status de cada teste, junto com carimbos de data/hora e mensagens detalhadas.
+                        Mantenha-se informado com notificações em tempo real sobre o status da sua rede. O aplicativo envia alertas diretamente para o seu dispositivo, garantindo que você esteja sempre ciente de quaisquer problemas.
+                        Você pode interromper os testes de rede a qualquer momento utilizando o interruptor. O aplicativo cessará todos os testes em andamento e atualizará o status conforme necessário.
+                    </p>
+                    <strong style={{ paddingLeft: '20px', paddingRight: '20px' }}>Faça Doações para Futuros Upgrades:</strong>
+                    <p style={{ color: 'white', textAlign: 'justify', paddingLeft: '20px', paddingRight: '20px', marginBottom: '20px' }}>
+                        Se você gostou do NetStabiliser e deseja apoiar seu desenvolvimento contínuo, considere fazer uma doação. Qualquer contribuição a partir de $2 USD será muito apreciada e nos ajudará a implementar novos recursos e melhorias. Faça sua doação através do PayPal para o seguinte endereço de e-mail: Justinojoserauanheque7@gmail.com. Obrigado por sua generosidade e apoio contínuo!
+                    </p>
+                    <button className="download-button" onClick={handleDownloadClick}>Download Arquivo</button>
+                </div>
             </div>
-            <button className="refresh-button" onClick={handleRefreshClick}>Refresh</button> {/* Adicionando o botão com a classe refresh-button e o evento de clique */}
+            <div className="countdown-container">
+                <button className="refresh-button" onClick={handleWhatsappClick}>Abrir WhatsApp</button>
+            </div>
         </div>
     );
 };
